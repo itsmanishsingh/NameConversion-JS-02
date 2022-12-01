6c40b4bba156952c4eec6aa6b11f82bf4dc86b07
itsmanishsingh <itsmanishsingh6@gmail.com>
2022-12-01 23:10:59 +0530

    project initiate

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Name Conversion
+***
+### In this I have tried to convert the input given by the user into different *<b>Naming Convention</b>* using core  ***JavaScript***.
+
+Different Naming Convention 
+
+
+1. Camel-Case
+1. Pascal Case
+1. Snake Case
+1. Screaming Snake Case
+1. Kebab Case
+1. Screaming Kebab Case
+
+
+# Output
+![output](./Image/output.png)
---
+++ nameConversion.js
@@ -0,0 +1,47 @@
+
+
+function myFunction() {
+  const word = document.getElementById("text").value;
+
+  document.getElementById("camel-case").innerHTML = camelCase(word);
+
+  function camelCase(str) {
+    return str
+      .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+        return index == 0 ? word.toLowerCase() : word.toUpperCase();
+      })
+      .replace(/\s+/g, "");
+  }
+
+  document.getElementById("pascal-case").innerHTML = PascalCase(word);
+
+  function PascalCase(str) {
+    return (' ' + str).toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => {
+        return chr.toUpperCase()});
+  }
+
+  document.getElementById("snake-case").innerHTML = SnakeCase(word);
+
+  function SnakeCase(str){
+    return str.replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+  }
+ 
+  
+  document.getElementById("screaming-snake-case").innerHTML = ScreamingSnakeCase(word);
+
+  function ScreamingSnakeCase(str){
+    return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => '-' + chr).trim();
+  }
+
+  document.getElementById("kebab-case").innerHTML = KebabCase(word);
+
+  function KebabCase(str){
+    return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => '-' + chr).trim();
+  }
+    function ScreamingkebabCase(str){
+      return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => '-' + chr).trim();
+    }
+  document.getElementById("screaming-kebab-case").innerHTML = ScreamingkebabCase(word);
+
+
+}
